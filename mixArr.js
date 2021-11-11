b11f345d46fa7394ec1f19ceb56bbae90ac93693
gary <gary.white@lodgit.com>
2021-11-11 13:13:18 +0100

    boilerplate

---
+++ mixArr.js
@@ -0,0 +1,7 @@
+function sumMix(x){
+
+}
+
+console.log(sumMix([9, 3, '7', '3'])); // Return 22
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7])); // Return 42
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])); // Return 41
